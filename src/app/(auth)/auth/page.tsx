671e174c9bf94abe878e04f2d26193f9bdb46b2a
Hiroya Nakano <hiroya_0610_15mro@me.com>
2025-07-14 18:13:32 +0900

    :sparkles: 認証コンポーネントの作成

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,7 @@
+import { AuthUser } from '@/components/ui/auth';
+
+const AuthPage = async () => {
+	return <AuthUser />;
+};
+
+export default AuthPage;
---
+++ src/components/ui/auth/auth-user.tsx
@@ -0,0 +1,3 @@
+export const AuthUser = () => {
+	return <p>Authentication User</p>;
+};
